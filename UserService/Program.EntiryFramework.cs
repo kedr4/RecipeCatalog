7ce1d45756d2d0e6af4830079de8dbdc468bb7c5
kedr4 <85400754+kedr4@users.noreply.github.com>
2024-11-11 21:40:59 +0300

    Add SqlServer

--- UserService/Program.EntiryFramework.cs
+++ UserService/Program.EntiryFramework.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using UserService.Infrastructure.Database;
 
 namespace UserService.Api
@@ -8,12 +9,13 @@ namespace UserService.Api
         public static IServiceCollection AddEntityFramework(this IServiceCollection services, IConfiguration configuration)
         {
             // Настройка SQL Server
-            // services.AddDbContext<DatabaseContext>(options =>
-            // options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DatabaseContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+            b => b.MigrationsAssembly("UserService.Infrastructure")));
 
             //in-memory DB
-            services.AddDbContext<DatabaseContext>(options =>
-               options.UseInMemoryDatabase("InMemoryDatabase"));
+            //services.AddDbContext<DatabaseContext>(options =>
+            //   options.UseInMemoryDatabase("InMemoryDatabase"));
 
             return services;
         }
